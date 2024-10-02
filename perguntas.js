db9adf05357d7e464570237c358eab8b304368b9
brunoaguiar07 <159555728+brunoaguiar07@users.noreply.github.com>
2024-10-02 08:04:43 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -24,169 +24,39 @@ criaCartao(
 
 criaCartao(
     'Programação',
-    'O que e robotica',
-    'E a ciencia que estuda a computacao e planejamento dos robos'
+    'Qual a Fórmula da Água?',
+    'A Fórmula da Água é H20'
 )
 
 criaCartao(
     'Geografia',
-    'Qual a capital do Brasil',
-    'A capital do Brasil e Brasilia'
+    'Qual a capital da Itália?,
+    'A capital da Itália é Roma'
 )
 
 criaCartao(
     'Programação',
-    'Qual o maior animal terrestre',
-    'O maior animal terrestre e o Elefante'
+    'Qual o maior animal do planeta?',
+    'O maior animal do planeta é a  Baleia-Azul'
 )
 
 criaCartao(
     'Lingua inglesa',
-    'Como se diz tchau em Inglês?',
-    'Tchau em ingles é Bye`
-)
+    'Como se diz verde em ingles',
+    'Verde em ingles é Green'
 
-criaCartao(
+)criaCartao(
     'Programação',
-    'O que e robotica',
-    'E a ciencia que estuda a computacao e planejamento dos robos'
-)
-
-criaCartao(
-    'Geografia',
-    'Qual a capital do Brasil',
-    'A capital do Brasil e Brasilia'
-)
-
-criaCartao(
-    'Programação',
-    'Qual o maior animal terrestre',
-    'O maior animal terrestre e o Elefante'
+    'Qual o maior peixe do mundo?',
+    'O maior peixe do planeta é o tubarão baleia'
 )
 
 criaCartao(
     'Lingua inglesa',
-    'Como se diz tchau em Inglês?',
-    'Tchau em ingles é Bye`
+    'Como se diz vermelho em ingles',
+    'Vermelho em ingles é Red'
+
 )
 
-criaCartao(
-    'Programação',
-    'O que e robotica',
-    'E a ciencia que estuda a computacao e planejamento dos robos'
-)
 
-criaCartao(
-    'Geografia',
-    'Qual a capital do Brasil',
-    'A capital do Brasil e Brasilia'
-)
-
-criaCartao(
-    'Programação',
-    'Qual o maior animal terrestre',
-    'O maior animal terrestre e o Elefante'
-)
-
-criaCartao(
-    'Lingua inglesa',
-    'Como se diz tchau em Inglês?',
-    'Tchau em ingles é Bye`
-)
-
-criaCartao(
-    'Programação',
-    'O que e robotica',
-    'E a ciencia que estuda a computacao e planejamento dos robos'
-)
-
-criaCartao(
-    'Geografia',
-    'Qual a capital do Brasil',
-    'A capital do Brasil e Brasilia'
-)
-
-criaCartao(
-    'Programação',
-    'Qual o maior animal terrestre',
-    'O maior animal terrestre e o Elefante'
-)
-
-criaCartao(
-    'Lingua inglesa',
-    'Como se diz tchau em Inglês?',
-    'Tchau em ingles é Bye`
-)
-
-criaCartao(
-    'Programação',
-    'O que e robotica',
-    'E a ciencia que estuda a computacao e planejamento dos robos'
-)
-
-criaCartao(
-    'Geografia',
-    'Qual a capital do Brasil',
-    'A capital do Brasil e Brasilia'
-)
-
-criaCartao(
-    'Programação',
-    'Qual o maior animal terrestre',
-    'O maior animal terrestre e o Elefante'
-)
-
-criaCartao(
-    'Lingua inglesa',
-    'Como se diz tchau em Inglês?',
-    'Tchau em ingles é Bye`
-)
-
-criaCartao(
-    'Programação',
-    'O que e robotica',
-    'E a ciencia que estuda a computacao e planejamento dos robos'
-)
-
-criaCartao(
-    'Geografia',
-    'Qual a capital do Brasil',
-    'A capital do Brasil e Brasilia'
-)
-
-criaCartao(
-    'Programação',
-    'Qual o maior animal terrestre',
-    'O maior animal terrestre e o Elefante'
-)
-
-criaCartao(
-    'Lingua inglesa',
-    'Como se diz tchau em Inglês?',
-    'Tchau em ingles é Bye`
-)
-
-criaCartao(
-    'Programação',
-    'O que e robotica',
-    'E a ciencia que estuda a computacao e planejamento dos robos'
-)
-
-criaCartao(
-    'Geografia',
-    'Qual a capital do Brasil',
-    'A capital do Brasil e Brasilia'
-)
-
-criaCartao(
-    'Programação',
-    'Qual o maior animal terrestre',
-    'O maior animal terrestre e o Elefante'
-)
-
-criaCartao(
-    'Lingua inglesa',
-    'Como se diz tchau em Inglês?',
-    'Tchau em ingles é Bye`
-)
 
